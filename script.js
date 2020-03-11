bc74b231e14db3c586033c2cf600690d6222e976
Semidán Robaina-Estévez <tintanjarafe@gmail.com>
2020-03-11 16:52:19 +0000

    using native leaflet watchlocation

--- script.js
+++ script.js
@@ -1,61 +1,92 @@
 let map, current_marker, circle;
 
-function initialize() {
-  navigator.geolocation.getCurrentPosition(function(pos) {
-    let latitude = pos.coords.latitude;
-    let longitude = pos.coords.longitude;
-    let radius = pos.coords.accuracy;
-    renderMap(latitude, longitude, radius);
+// function initialize() {
+//   navigator.geolocation.getCurrentPosition(function(pos) {
+//     let latitude = pos.coords.latitude;
+//     let longitude = pos.coords.longitude;
+//     let radius = pos.coords.accuracy;
+//     renderMap(latitude, longitude, radius);
+//
+//     // let locate_options = {
+//     //   "watch": true,
+//     //   "setView": true,
+//     //   "enableHighAccuracy": true
+//     // }
+//     // map.locate(locate_options);
+//
+//
+//     displayText(latitude, longitude, radius);
+//    }
+//   );
+//
+//
+//   let text_container = document.getElementById("text_container");
+//   for (let key of Object.keys(data)) {
+//     data[key].showed = false;
+//     let div = document.createElement("div");
+//     div.setAttribute("class", "text-popup");
+//     div.setAttribute("id", key);
+//     let title = document.createElement("h2");
+//     title.setAttribute("class", "text-title");
+//     title.innerHTML = data[key].tag;
+//     div.appendChild(title);
+//     let img = document.createElement("img");
+//     img.setAttribute("class", "text-image");
+//     img.setAttribute("src", data[key].img);
+//     div.appendChild(img);
+//     let p = document.createElement("p");
+//     p.innerHTML = data[key].text;
+//     div.appendChild(p);
+//     text_container.appendChild(div);
+//   }
+// }
 
-    // let locate_options = {
-    //   "watch": true,
-    //   "setView": true,
-    //   "enableHighAccuracy": true
-    // }
-    // map.locate(locate_options);
 
+// function initialize() {
+//
+//   if (navigator.geolocation) {
+//     navigator.geolocation.watchPosition(function(pos){
+//       let latitude = pos.coords.latitude;
+//       let longitude = pos.coords.longitude;
+//       let radius = pos.coords.accuracy;
+//       renderMap(latitude, longitude, radius);
+//       displayText(latitude, longitude, radius);
+//     });
+//   } else {
+//     alert("Geolocation is not supported by this browser.");
+//   }
+//
+//   let text_container = document.getElementById("text_container");
+//   for (let key of Object.keys(data)) {
+//     data[key].showed = false;
+//     let div = document.createElement("div");
+//     div.setAttribute("class", "text-popup");
+//     div.setAttribute("id", key);
+//     let title = document.createElement("h2");
+//     title.setAttribute("class", "text-title");
+//     title.innerHTML = data[key].tag;
+//     div.appendChild(title);
+//     let img = document.createElement("img");
+//     img.setAttribute("class", "text-image");
+//     img.setAttribute("src", data[key].img);
+//     div.appendChild(img);
+//     let p = document.createElement("p");
+//     p.innerHTML = data[key].text;
+//     div.appendChild(p);
+//     text_container.appendChild(div);
+//   }
+// }
 
-    displayText(latitude, longitude, radius);
-   }
-  );
-  let text_container = document.getElementById("text_container");
-  for (let key of Object.keys(data)) {
-    data[key].showed = false;
-    let div = document.createElement("div");
-    div.setAttribute("class", "text-popup");
-    div.setAttribute("id", key);
-    let title = document.createElement("h2");
-    title.setAttribute("class", "text-title");
-    title.innerHTML = data[key].tag;
-    div.appendChild(title);
-    let img = document.createElement("img");
-    img.setAttribute("class", "text-image");
-    img.setAttribute("src", data[key].img);
-    div.appendChild(img);
-    let p = document.createElement("p");
-    p.innerHTML = data[key].text;
-    div.appendChild(p);
-    text_container.appendChild(div);
-  }
-}
+function initializeMap() {
 
-function renderMap(latitude, longitude, radius) {
-
-  map = L.map('map').setView([latitude, longitude], 15);
+  map = L.map('map');
 
   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
   }).addTo(map);
 
-  current_marker = L.marker([latitude, longitude]).addTo(map);/*
-      .bindPopup('You are here!.')
-      .openPopup();*/
-
-  circle = L.circle([latitude, longitude], {radius: radius}, {
-  		color: 'blue',
-  		fillColor: 'rgb(86, 155, 227)',
-  		fillOpacity: 0.5
-  	}).addTo(map);//.bindPopup("I am a circle.");
+  current_marker = L.marker([0, 0]).addTo(map);
+  circle = L.circle([0, 0], {radius: 1}).addTo(map);
 
   let greenIcon = new L.Icon({
     iconUrl: 'images/green_marker.png',
@@ -69,34 +100,62 @@ function renderMap(latitude, longitude, radius) {
   for (let loc of Object.keys(data)) {
     L.marker(data[loc].coords, {icon: greenIcon}).addTo(map);
   }
+
+  map.locate({setView: true,
+             maxZoom: 16,
+             watch:true
+           });
+  map.on('locationfound', updateMap);
 }
 
-function updateMap() {
-  navigator.geolocation.getCurrentPosition(function(pos) {
+// function updateMap() {
+//   navigator.geolocation.getCurrentPosition(function(pos) {
+//
+//     let latitude = pos.coords.latitude;
+//     let longitude = pos.coords.longitude;
+//     let radius = pos.coords.accuracy;
+//     map.removeLayer(current_marker);
+//     map.removeLayer(circle);
+//
+//     current_marker = L.marker([latitude, longitude]).addTo(map);
+//     circle = L.circle([latitude, longitude], {radius: radius}, {
+//         color: 'blue',
+//         fillColor: 'rgb(86, 155, 227)',
+//         fillOpacity: 0.5
+//       }).addTo(map);
+//
+//     map.setView([latitude, longitude], 18);
+//     displayText(latitude, longitude, radius);
+//    }
+//  );
+// }
 
-    let latitude = pos.coords.latitude;
-    let longitude = pos.coords.longitude;
-    let radius = pos.coords.accuracy;
-    map.removeLayer(current_marker);
-    map.removeLayer(circle);
+function updateMap(pos) {
+  console.log(pos.latlng);
+  let radius = pos.accuracy / 2;
+  map.removeLayer(current_marker);
+  map.removeLayer(circle);
 
-    current_marker = L.marker([latitude, longitude]).addTo(map);
-    circle = L.circle([latitude, longitude], {radius: radius}, {
-        color: 'blue',
-        fillColor: 'rgb(86, 155, 227)',
-        fillOpacity: 0.5
-      }).addTo(map);
-
-    map.setView([latitude, longitude], 15);
-    displayText(latitude, longitude, radius);
-   }
- );
+  current_marker = L.marker(pos.latlng).addTo(map);
+  circle = L.circle(pos.latlng, {radius: radius}, {
+  		color: 'blue',
+  		fillColor: 'rgb(86, 155, 227)',
+  		fillOpacity: 0.5
+  	}).addTo(map);
+  map.setView(pos.latlng, 18);
+  displayText(pos.latlng, radius);
 }
 
-function displayText(latitude, longitude, radius) {
-  let limit = 20; // meters
-  let current_coords = [latitude, longitude];
+// function UpdateLocation() {
+//   if (navigator.geolocation) {
+//     navigator.geolocation.watchPosition(updateMap);
+//   } else {
+//     x.innerHTML = "Geolocation is not supported by this browser.";
+//   }
+// }
 
+function displayText(current_coords, radius) {
+  let limit = 30; // meters
   let texts = document.getElementsByClassName("text-popup");
   for (let text of texts) {
     text.style.display = "none";
@@ -116,9 +175,9 @@ function displayText(latitude, longitude, radius) {
   }
 }
 
-function euclidean(x, y) {
-  return Math.sqrt((x[0] - y[0])**2 + (x[1] - y[1])**2)
-}
+// function euclidean(x, y) {
+//   return Math.sqrt((x[0] - y[0])**2 + (x[1] - y[1])**2)
+// }
 
 function toRadians(degrees) {
   return degrees * (Math.PI / 180);
@@ -143,4 +202,4 @@ function haversine(x, y) {
   return distance_in_meters
 }
 
-setInterval(function(){ updateMap(); }, 5 * 1000);
+//setInterval(function(){ updateMap(); }, 5 * 1000);
